8a140da539744d36e73252fe4ae13543e4ee50df
RolfNoot <smits@wth.nl>
2023-01-26 17:05:25 +0100

    several fixes, enhanced OTX variables replacement

--- .vscode/otxC.js
+++ .vscode/otxC.js
@@ -1,22 +1,21 @@
 //import * as logging from '@cmt/logging';
-var output;
+const vscode = require("vscode");
 
 exports.execute = async (args) => {
     var ret = null;
-    const vscode = args.require('vscode');
     switch (args.command)
     {
         case 'otx.preLaunch':
-            ret = await otxBuild(vscode); 
+            ret = await otxBuild(); 
             break;
         case 'otx.clean':
-            ret = await otxClean(vscode);
+            ret = await otxClean();
             break;
         case 'otx.build':
-            ret = await otxBuild(vscode);
+            ret = await otxBuild();
             break;
         case 'otx.run':
-            ret = await otxRun(vscode);
+            ret = await otxRun();
             break;
     }
     return ret;
@@ -27,8 +26,8 @@ function sleep(ms) {
   }
 
 
-otxPreLaunch = async (vscode) => {
-    var ret = await executeTask(vscode, "Meson: build");
+otxPreLaunch = async () => {
+    var ret = await executeTask("Meson: build");
     if (ret == 0) return '';
     vscode.window.showErrorMessage("The build task terminated with exit code:" + JSON.stringify(ret));
     vscode.commands.executeCommand('workbench.action.problems.focus');
@@ -36,10 +35,10 @@ otxPreLaunch = async (vscode) => {
     return null;
 };
 
-otxPreLaunchOLD = async (vscode) => {
+otxPreLaunchOLD = async () => {
     //var ret = await vscode.commands.executeCommand("mesonbuild.build", "");
     var ret = null;
-    ret = await runMesonBuild(vscode, vscode.workspace.workspaceFolders[0].uri.path, "Debug");
+    ret = await runMesonBuild(vscode.workspace.workspaceFolders[0].uri.path, "Debug");
 
      if (ret == '0') return '';
      // vscode.commands.executeCommand('workbench.panel.output.focus', 'Adapter Output');
@@ -56,22 +55,22 @@ otxPreLaunchOLD = async (vscode) => {
     // return null;
 };
 
-otxClean = async (vscode) => {
-    let { status, basePath } = await checkSetup(vscode);
+otxClean = async () => {
+    let { status, basePath } = await checkSetup();
     if (status == 'error') 
     {
         vscode.window.showErrorMessage("The build task terminated with exit code:" + JSON.stringify(ret));
         return null;
     }
     if (status == 'configured') return '';
-    var ret = await executeTask(vscode, "Meson: clean-reconfigure");
+    var ret = await executeTask("Meson: clean-reconfigure");
     if (ret == 0) return '';
     vscode.window.showErrorMessage("The build task terminated with exit code:" + JSON.stringify(ret));
     return null;
 };
 
-otxBuild = async (vscode) => {
-    let { status, basePath } =  await checkSetup(vscode);
+otxBuild = async () => {
+    let { status, basePath } =  await checkSetup();
     if (status == 'error') 
     {
         vscode.window.showErrorMessage("The build task terminated with exit code:" + JSON.stringify(ret));
@@ -88,7 +87,7 @@ otxBuild = async (vscode) => {
     var sourceContents = readDirectory(basePath, [], sourcePath, '.c', false);
 
     updateMeson(mesonBuildFile, headerContents, sourceContents);
-    var ret = await executeTask(vscode, "Meson: build");
+    var ret = await executeTask("Meson: build");
     if (ret == 0) return '';
     vscode.window.showErrorMessage("The build task terminated with exit code:" + JSON.stringify(ret));
     vscode.commands.executeCommand('workbench.action.problems.focus');
@@ -96,7 +95,7 @@ otxBuild = async (vscode) => {
     return null;
 }
 
-async function executeTask(vscode, taskName)
+async function executeTask(taskName)
 {   
     var tasks = await vscode.tasks.fetchTasks();
     var task = undefined;
@@ -117,7 +116,7 @@ async function executeTask(vscode, taskName)
     });
 }
 
-async function executeBuildTask(vscode) {
+async function executeBuildTask() {
     var problemMatchers = [{
         owner: "linker1",
         severity: "error",
@@ -169,7 +168,7 @@ async function executeBuildTask(vscode) {
     // });
 }
 
-async function executeCleanTask(vscode) {
+async function executeCleanTask() {
     let buildTask = 
     new vscode.Task(
         { type: 'Meson Build' },
@@ -187,7 +186,7 @@ async function executeCleanTask(vscode) {
         });
 }
 
-async function getBuildTasks(vscode) {
+async function getBuildTasks() {
     return new Promise<vscode.Task(resolve => {
         vscode.tasks.fetchTasks().then((tasks) => {
             resolve(tasks.filter((task) => task.group === vscode.TaskGroup.Build));
@@ -219,7 +218,7 @@ async function getBuildTasks(vscode) {
 //     return null;
 
 
-otxRun = async (vscode) => {
+otxRun = async () => {
     //var ret = await vscode.commands.executeCommand('workbench.action.debug.run');
     //var ret = await vscode.commands.executeCommand('workbench.action.debug.selectandstart');
     var ret = await vscode.commands.executeCommand('workbench.action.debug.start');
@@ -232,7 +231,7 @@ otxRun = async (vscode) => {
 const fs = require("fs");
 const path = require("path");
 
-async function checkSetup(vscode)
+async function checkSetup()
 {
     var basePath = "";
     if(vscode.workspace.workspaceFolders === undefined)
@@ -246,7 +245,7 @@ async function checkSetup(vscode)
     {
         if (!fs.existsSync(buildDir))
             await fs.promises.mkdir(buildDir)
-        var ret = await executeTask(vscode, "Meson: configure");
+        var ret = await executeTask("Meson: configure");
         return { 'status': 'configured', 'basePath': basePath };
     }
     return { 'status': 'ok', 'basePath': basePath };
@@ -282,20 +281,47 @@ function readDirectory(basePath, refArray, dir, extension, foldersOnly) {
     return refArray;
 }
 
+var process = require('process');
 function updateMeson(mesonFile, headerContents, sourceContents) {
-	const mesonContents = fs.readFileSync(mesonFile, 'utf-8');
+    const mesonContents = fs.readFileSync(mesonFile, 'utf-8');
 	var arr = [];
-	var logOut = true;
+	var logOut = true; var linesStripped = 0;
 	mesonContents.split(/\r?\n/).forEach((line) => {
 		if (line.includes("OTX_Extension_HeaderFiles_End") || line.includes("OTX_Extension_SourceFiles_End")) logOut = true;
 		if (logOut) arr.push(line);
+        if (linesStripped > 0 && --linesStripped == 0) logOut = true;
 		if (line.includes("OTX_Extension_HeaderFiles_Start")) {
 			arr = arr.concat(headerContents);
 			logOut = false;
 		}
-		if (line.includes("OTX_Extension_SourceFiles_Start")) {
+		else if (line.includes("OTX_Extension_SourceFiles_Start")) {
 			arr = arr.concat(sourceContents);
 			logOut = false;
+		}
+        else if (line.includes("OTX_Extension_print")) {
+            const regexp = /\(\s*(.*[^ ])[ )]+$/;
+            var array = line.match(regexp);
+            if (array != null)
+                arr = arr.concat(substituteVariables(array[1]));
+            else
+                arr = arr.concat('Not found!');
+			logOut = false; linesStripped = 1;
+		}
+        else if (line.includes("OTX_Extension_getEnv")) {
+            const regexp = /\(\s*(\w*).*,\s*(\w*).*\)/;
+            var array = line.match(regexp);
+            array = (array === null)? ["Not found!", ""] : [array[1], process.env[array[2]]];
+            arr = arr.concat(array[0] + " = '" + array[1] + "'");
+			logOut = false; linesStripped = 1;
+		}
+        else if (line.includes("OTX_Extension_getVariable")) {
+            const regexp = /\(\s*([^ ]+).*,\s*([^ ]+).*\)/;
+            var array = line.match(regexp);
+            if (array != null)
+                arr = arr.concat(array[1] + " = '" + substituteVariables(array[2]) + "'");
+            else
+                arr = arr.concat('Not found!');
+			logOut = false; linesStripped = 1;
 		}
 	});
 	const contents= arr.join('\n');
@@ -307,10 +333,10 @@ function updateMeson(mesonFile, headerContents, sourceContents) {
 
 
 
-async function runMesonBuild(vscode, buildDir, name) {
+async function runMesonBuild(buildDir, name) {
 
     try {
-        let buildTask = await getTask(vscode, "build", name);
+        let buildTask = await getTask("build", name);
         const buildTaskExecution = await vscode.tasks.executeTask(buildTask);
         return new Promise((resolve) => {
             vscode.tasks.onDidEndTaskProcess(e => {
@@ -360,7 +386,7 @@ async function runMesonBuild(vscode, buildDir, name) {
 //     });
 //   }
 
-  async function getTask(vscode, mode, name) {
+  async function getTask(mode, name) {
     const tasks = await vscode.tasks.fetchTasks({ type: "meson" });
     const filtered = tasks.filter(
       t => t.definition.mode === mode && (!name || t.definition.target === name)
@@ -369,3 +395,165 @@ async function runMesonBuild(vscode, buildDir, name) {
       throw new Error(`Cannot find ${mode} target ${name}.`);
     return filtered[0];
   }
+
+// https://raw.githubusercontent.com/usernamehw/vscode-commands/master/src/substituteVariables.ts
+// https://github.com/microsoft/vscode/blob/main/src/vs/workbench/services/configurationResolver/common/variableResolver.ts
+
+// TODO: ${userHome}
+const VariableNames = {
+	File : '${file}', // the current opened file (absolute path?)
+	FileBasename : '${fileBasename}', // the current opened file's basename
+	FileBasenameNoExtension : '${fileBasenameNoExtension}', // the current opened file's basename with no file extension
+	FileExtname : '${fileExtname}', // the current opened file's extension
+	FileDirname : '${fileDirname}', // the current opened file's dirname
+	FileWorkspaceFolder : '${fileWorkspaceFolder}', // the current opened file's workspace folder
+	WorkspaceFolder : '${workspaceFolder}', // the path of the folder opened in VS Code
+	WorkspaceFolderBasename : '${workspaceFolderBasename}', // the name of the folder opened in VS Code without any slashes (/)
+	ExecPath : '${execPath}', //  location of Code.exe
+	PathSeparator : '${pathSeparator}', // `/` on macOS or linux, `\` on Windows
+	LineNumber : '${lineNumber}', // the current selected line number in the active file
+	SelectedText : '${selectedText}', // the current selected text in the active file
+	EnvironmentVariable : '${env}',
+	SingleEnvironmentVariable : 'env',
+	ConfigurationVariable : '${config}',
+	SingleConfigurationVariable : 'config',
+	// ────────────────────────────────────────────────────────────
+	// relativeFile : '${relativeFile}', // the current opened file relative to `workspaceFolder`
+	// relativeFileDirname : '${relativeFileDirname}', // the current opened file's dirname relative to `workspaceFolder`
+	// cwd : '${cwd}', // the task runner's current working directory on startup
+}
+
+const variableRegexps = {
+	[VariableNames.File]: new RegExp(escapeRegExp(VariableNames.File), 'ig'),
+	[VariableNames.FileBasename]: new RegExp(escapeRegExp(VariableNames.FileBasename), 'ig'),
+	[VariableNames.FileBasenameNoExtension]: new RegExp(escapeRegExp(VariableNames.FileBasenameNoExtension), 'ig'),
+	[VariableNames.FileDirname]: new RegExp(escapeRegExp(VariableNames.FileDirname), 'ig'),
+	[VariableNames.FileExtname]: new RegExp(escapeRegExp(VariableNames.FileExtname), 'ig'),
+	[VariableNames.FileWorkspaceFolder]: new RegExp(escapeRegExp(VariableNames.FileWorkspaceFolder), 'ig'),
+	[VariableNames.WorkspaceFolder]: new RegExp(escapeRegExp(VariableNames.WorkspaceFolder), 'ig'),
+	[VariableNames.WorkspaceFolderBasename]: new RegExp(escapeRegExp(VariableNames.WorkspaceFolderBasename), 'ig'),
+	[VariableNames.ExecPath]: new RegExp(escapeRegExp(VariableNames.ExecPath), 'ig'),
+	[VariableNames.PathSeparator]: new RegExp(escapeRegExp(VariableNames.PathSeparator), 'ig'),
+	[VariableNames.LineNumber]: new RegExp(escapeRegExp(VariableNames.LineNumber), 'ig'),
+	[VariableNames.SelectedText]: new RegExp(escapeRegExp(VariableNames.SelectedText), 'ig'),
+	[VariableNames.SingleEnvironmentVariable]: /\${env:([a-zA-Z_]+[a-zA-Z0-9_]*)}/i,
+	[VariableNames.EnvironmentVariable]: /\${env:([a-zA-Z_]+[a-zA-Z0-9_]*)}/ig,
+	[VariableNames.SingleConfigurationVariable]: /\${config:([^}]+?)}/i,
+	[VariableNames.ConfigurationVariable]: /\${config:([^}]+?)}/ig,
+	// [VariableNames.relativeFile]: new RegExp(escapeRegExp(VariableNames.relativeFile), 'ig'),
+	// [VariableNames.relativeFileDirname]: new RegExp(escapeRegExp(VariableNames.relativeF
+};
+
+function escapeRegExp(string) {
+    return string.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+/**
+ * Try to emulate variable substitution in tasks https://code.visualstudio.com/docs/editor/variables-reference
+ *
+ * TODO: throw errors (window.showMessage) when variable exists but can't resolve
+ */
+
+function substituteVariables2(str) {
+    const activeTextEditor = vscode.window.activeTextEditor;
+    const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+    if (str.includes(VariableNames.WorkspaceFolder) && workspaceFolder) {
+		str = str.replace(variableRegexps[VariableNames.WorkspaceFolder], workspaceFolder);
+	}
+    return str;
+}
+function substituteVariables(str) {
+	const activeTextEditor = vscode.window.activeTextEditor;
+	const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+	if (str.includes(VariableNames.SelectedText) && activeTextEditor) {
+		const selection = activeTextEditor.selection;
+		const selectedText = activeTextEditor.document.getText(selection);
+		str = str.replace(variableRegexps[VariableNames.SelectedText], selectedText);
+	}
+	if (str.includes(VariableNames.PathSeparator)) {
+		str = str.replace(variableRegexps[VariableNames.PathSeparator], path.sep);
+	}
+	if (str.includes(VariableNames.LineNumber) && activeTextEditor) {
+		str = str.replace(variableRegexps[VariableNames.LineNumber], String(activeTextEditor.selection.active.line + 1));
+	}
+	if (str.includes(VariableNames.ExecPath)) {
+		str = str.replace(variableRegexps[VariableNames.ExecPath], vscode.env.appRoot);
+	}
+	if (str.includes(VariableNames.File) && activeTextEditor) {
+		str = str.replace(variableRegexps[VariableNames.File], activeTextEditor.document.uri.fsPath);
+	}
+	if (str.includes(VariableNames.FileBasename) && activeTextEditor) {
+		str = str.replace(variableRegexps[VariableNames.FileBasename], path.basename(activeTextEditor.document.uri.fsPath));
+	}
+	if (str.includes(VariableNames.FileBasenameNoExtension) && activeTextEditor) {
+		str = str.replace(variableRegexps[VariableNames.FileBasenameNoExtension], path.basename(activeTextEditor.document.uri.fsPath, path.extname(activeTextEditor.document.uri.fsPath)));
+	}
+	if (str.includes(VariableNames.FileExtname) && activeTextEditor) {
+		str = str.replace(variableRegexps[VariableNames.FileExtname], path.extname(activeTextEditor.document.uri.fsPath));
+	}
+	if (str.includes(VariableNames.FileDirname) && activeTextEditor) {
+		str = str.replace(variableRegexps[VariableNames.FileDirname], path.dirname(activeTextEditor.document.uri.fsPath));
+	}
+	if (str.includes(VariableNames.WorkspaceFolder) && workspaceFolder) {
+		str = str.replace(variableRegexps[VariableNames.WorkspaceFolder], workspaceFolder);
+	}
+	if (str.includes(VariableNames.WorkspaceFolderBasename) && workspaceFolder) {
+		str = str.replace(variableRegexps[VariableNames.WorkspaceFolderBasename], path.basename(workspaceFolder));
+	}
+	if (str.includes(VariableNames.FileWorkspaceFolder) && activeTextEditor && workspaceFolder) {
+		const fileWorkspaceFolder = vscode.workspace.getWorkspaceFolder(activeTextEditor.document.uri)?.uri.fsPath;
+		if (fileWorkspaceFolder) {
+			str = str.replace(variableRegexps[VariableNames.FileWorkspaceFolder], fileWorkspaceFolder);
+		}
+	}
+	if (variableRegexps[VariableNames.EnvironmentVariable].test(str)) {
+		const match = str.match(variableRegexps[VariableNames.EnvironmentVariable]);
+
+		for (const _ of match || []) {
+			str = str.replace(variableRegexps[VariableNames.SingleEnvironmentVariable], (__, g1) => process.env[g1] || g1);
+		}
+	}
+	if (variableRegexps[VariableNames.ConfigurationVariable].test(str)) {
+		const match = str.match(variableRegexps[VariableNames.ConfigurationVariable]);
+
+		for (const _ of match || []) {
+			str = str.replace(variableRegexps[VariableNames.SingleConfigurationVariable], (__, g1) => replaceConfigurationVariable(g1));
+		}
+	}
+	return str;
+}
+
+function replaceConfigurationVariable(configName) {
+	if (!configName.includes('.')) {
+		vscode.window.showErrorMessage(`Need a dot (.) in the name of configuration. "${configName}"`);
+		return configName;
+	}
+	const configParts = configName.split('.');
+	const configValue = vscode.workspace.getConfiguration(configParts[0]).get(configParts.slice(1).join('.'));
+	if (typeof configValue !== 'string' && typeof configValue !== 'number') {
+		vscode.window.showErrorMessage(`Configuration must be of type: string or number "${configName}"`);
+		return configName;
+	}
+	return String(configValue);
+}
+
+/**
+ * Walk recursively through object/array and replace variables in strings.
+ */
+function substituteVariableRecursive(arg) {
+	if (typeof arg === 'string') {
+		return substituteVariables(arg);
+	}
+
+	if (Array.isArray(arg)) {
+		for (const [key, value] of arg.entries()) {
+			arg[key] = substituteVariableRecursive(value);
+		}
+	} else if (typeof arg === 'object' && arg !== null) {
+		for (const key in arg) {
+			// @ts-ignore
+			arg[key] = substituteVariableRecursive(arg[key]);
+		}
+	}
+
+	return arg;
+}
